b18290c09ae6a7b9fc111e521d24253cc028a323
jorgendo43 <jorge.verduzco247253@potros.itson.edu.mx>
2025-11-27 04:01:57 -0700

    error de bugs

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ const { globalErrorHandler } = require('./utils/appError.js');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // ---------------------------------------------------------
 // 1. ARCHIVOS ESTÁTICOS (Lo más importante para tu error)
